174f13182e87e0a471c0ed7931aa13377519d212
Damian Melniczuk <damian.melniczuk@gmail.com>
2024-07-13 16:06:30 +0200

    added ponds generation

--- scripts/BearCamera.gd
+++ scripts/BearCamera.gd
@@ -5,3 +5,4 @@ func _ready():
 	camera.limit_left = 0
 	camera.limit_right = 160
 	camera.limit_top = 0
+	camera.limit_bottom = 1600
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,10 +6,13 @@ extends Node2D
 @onready var enemy_container = $EnemyContainer
 @onready var ui = $UI
 @onready var tile_map: TileMap = $TileMap
-const TREE_CHANCE = 0.1  # 10% szans na wygenerowanie drzewa na każdym kafelku
+
 @onready var bee_container = $BeeContainer
 var bee_scene = preload("res://scenes/Bee.tscn")
-const BEE_CHANCE = 0.05  # 5% szans na wygenerowanie pszczoły na każdym wolnym kafelku
+const POND_SOURCE_ID = 0
+const POND_CHANCE = 0.003  # 3% szans na wygenerowanie jeziorka
+const BEE_CHANCE = 0.005
+const TREE_CHANCE = 0.1
 #var food_scene = preload("res://scenes/Food.tscn")
 #var obstacle_scene = preload("res://scenes/Obstacle.tscn")
 #var enemy_scene = preload("res://scenes/Enemy.tscn")
@@ -19,6 +22,7 @@ func _ready():
 
 func _process(delta):
 	pass
+	#print(bear.position)
 	#update_season()
 	#update_ui()
 
@@ -38,21 +42,66 @@ func get_random_transform(horizontal_only=false) -> int:
 	return transforms[randi() % transforms.size()]
 
 func spawn_initial_objects():
-	var tiles_list = [Vector2i(0,0), Vector2i(1,0), Vector2i(2,0)]
-	for x in range(10):
-		for y in range(1,100):
-			tile_map.set_cell(0, Vector2i(x, y), 0, tiles_list.pick_random(), get_random_transform())
-			
-	for x in range(10):
-		for y in range(1,99):
-			if randf() < TREE_CHANCE and is_space_for_tree(x, y):
-				spawn_tree(x, y)
+	var ponds_needed = 3
+	var current_ponds
+	var regenerate = true
+	var grass_tiles = [Vector2i(0,0), Vector2i(1,0), Vector2i(2,0)]
+	
+	while regenerate:
+		current_ponds = 0
+		for x in range(10):
+			for y in range(1,100):
+				# Generowanie trawy
+				tile_map.set_cell(0, Vector2i(x, y), 0, grass_tiles.pick_random(), get_random_transform())
 				
-	# Spawn pszczół
-	for x in range(10):
-		for y in range(1, 99):
-			if randf() < BEE_CHANCE and is_space_free(x, y):
-				spawn_bee(x, y)
+				if y > 98:
+					continue
+				if y >= 25:  # Generowanie jeziorek od 25 rzędu w dół
+					if randf() < POND_CHANCE and is_space_for_pond(x, y):
+						spawn_pond(x, y)
+						current_ponds += 1
+						if current_ponds >= ponds_needed:
+							regenerate = false
+					elif randf() < BEE_CHANCE and is_space_free(x, y):
+						spawn_bee(x, y)
+					elif randf() < TREE_CHANCE and is_space_for_tree(x, y):
+						spawn_tree(x, y)
+				else:
+					if randf() < BEE_CHANCE and is_space_free(x, y):
+						spawn_bee(x, y)
+					elif randf() < TREE_CHANCE and is_space_for_tree(x, y):
+						spawn_tree(x, y)
+			spawn_tree(x, 100)
+
+	
+	
+	#for x in range(10):
+		#for y in range(1,100):
+			#tile_map.set_cell(0, Vector2i(x, y), 0, tiles_list.pick_random(), get_random_transform())
+			#
+	#for x in range(10):
+		#for y in range(1,98):
+			#if randf() < TREE_CHANCE and is_space_for_tree(x, y):
+				#spawn_tree(x, y)
+		#spawn_tree(x, 100)
+	## Spawn pszczół
+	#for x in range(10):
+		#for y in range(1, 99):
+			#if randf() < BEE_CHANCE and is_space_free(x, y):
+				#spawn_bee(x, y)
+
+func is_space_for_pond(x: int, y: int) -> bool:
+	for dx in range(3):
+		for dy in range(3):
+			if tile_map.get_cell_source_id(1, Vector2i(x + dx, y + dy)) != -1 and tile_map.get_cell_source_id(0, Vector2i(x + dx, y + dy)) == -1:
+				return false
+	return true
+
+func spawn_pond(x: int, y: int):
+	for dx in range(3):
+		for dy in range(3):
+			var pond_tile = Vector2i(dx, dy + 1)  # +1 because pond tiles start from 0,1 to 2,3
+			tile_map.set_cell(1, Vector2i(x + dx, y + dy), POND_SOURCE_ID, pond_tile)
 
 func is_space_free(x: int, y: int) -> bool:
 	return tile_map.get_cell_source_id(1, Vector2i(x, y)) == -1
